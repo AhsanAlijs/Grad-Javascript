80ef11c14531ba6c82946e023591565c9061da7d
Ahsan-ali001 <ahsanali45444@gmail.com>
2023-08-02 10:40:48 -0700

    grad project is redy

---
+++ app.js
@@ -0,0 +1,17 @@
+var percantage = prompt("Enter Yor Percantage");
+// console.log (percantage);
+if (percantage >=80) {
+    console.log("Your Grad is A+")
+
+} else if(percantage >=70){
+    console.log("your grad is A")
+}else if(percantage >=60){
+    console.log("your grad is B")
+}else if(percantage >=50){
+    console.log("your grad is C")
+}else if(percantage >=40){
+    console.log("your grad is D")
+}
+else {
+    console.log("Sorry Batter Luck Next Time");
+}
